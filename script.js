66324e8a26e4f1de43631031d615b1b3cc340421
Aron Kabai Tokes <aronkt3458@hotmail.com>
2024-04-29 21:37:22 -0400

    created midpoint rule calculations

--- script.js
+++ script.js
@@ -1,14 +1,86 @@
 $(document).ready(function() {
 
-  $('.inputs').submit(function() {
-    var $inputs = $('.inputs : input');
+  $('.inputs').submit(function(e) {
+    e.preventDefault();
 
-    var values = {};
-    $inputs.each(function() {
-      values[this.name] = $(this).val();
-    });
+    // Get all the forms elements and their values 
+    let func = $('#function').val();
+    let getLowerLimit = $('#lowerLimit').val();
+    let getUpperLimit = $('#upperLimit').val();
+    let getSubIntervals = $('#subIntervals').val();
 
-    console.log(values);
+    // typecasting the inputs to their respective identities
+    let equation = math.compile(func);
+    let lowerLimit = Number(getLowerLimit);
+    let upperLimit = Number(getUpperLimit);
+    let subIntervals = Number(getSubIntervals);
+    let isNegative = false
+
+    // if the upper limit is smaller than the lower limit, swap them and
+    // make the equation negative
+    if (upperLimit <= lowerLimit) {
+      isNegative = true;
+      let temp = upperLimit;
+      upperLimit = lowerLimit;
+      lowerLimit = temp;
+    }
+
+    let deltaX = (upperLimit - lowerLimit) / subIntervals;
+
+    let left = lowerLimit;
+
+    let right = lowerLimit + deltaX;
+
+    let answer = 0;
+
+    let finalAnswer;
+    while (left < upperLimit) {
+      
+      let currentMidpoint = (left + right) / 2
+      answer += math.evaluate({ x: currentMidpoint });
+
+      left = right;
+      right = right + h;
+
+      
+     
+      
+    }
+    
+    finalAnswer = deltaX * answer;
+    if (isNegative) {
+      finalAnswer *= -1;
+    }
+
+    console.log("delta x: " + deltaX);
+    console.log("left: " + left);
+    console.log("right: " + right);
+    console.log("final answer: "+ finalAnswer);
+   
+    console.log("THE VALUES:");
+    console.log(func);
+    console.log(getLowerLimit);
+    console.log(getUpperLimit);
+    console.log(getSubIntervals);
+
+    console.log('\nTHE VALUES IN MATH FORM:');
+    console.log(equation);
+    console.log(lowerLimit);
+    console.log(upperLimit);
+    console.log(subIntervals);
+
+    // displaying the value
+    
+    console.log(finalAnswer);
+    $("#answer").text(finalAnswer);
+
+    // $('#answer').displayValue(finalAnswer); 
   });
 
+  // jQuery.fn.displayValue = function(finalAnswer) {
+    // console.log("displaying value!");
+    // $(this).text(finalAnswer);
+    // console.log(finalAnswer);
+    // console.log(this.length);
+  // }
 });
