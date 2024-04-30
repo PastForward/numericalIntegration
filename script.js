aa767c3a06cf9e9578a3cdb88b8195650e422d05
Aron Kabai Tokes <aronkt3458@hotmail.com>
2024-04-29 23:27:28 -0400

    fixed math.evaluate not working

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
+  
+    $('.inputs').submit(function(e) {
 
-  $('.inputs').submit(function(e) {
+    // prevents the submission from reloading the page
     e.preventDefault();
 
     // Get all the forms elements and their values 
@@ -8,9 +10,9 @@ $(document).ready(function() {
     let getLowerLimit = $('#lowerLimit').val();
     let getUpperLimit = $('#upperLimit').val();
     let getSubIntervals = $('#subIntervals').val();
-
+    
     // typecasting the inputs to their respective identities
-    let equation = math.compile(func);
+    const equation = math.compile(func);
     let lowerLimit = Number(getLowerLimit);
     let upperLimit = Number(getUpperLimit);
     let subIntervals = Number(getSubIntervals);
@@ -24,27 +26,35 @@ $(document).ready(function() {
       upperLimit = lowerLimit;
       lowerLimit = temp;
     }
-
+    
+    // calculating the delta x for the midpoint
     let deltaX = (upperLimit - lowerLimit) / subIntervals;
 
+    // the x0 (left side) of the equation that is (x0 + x1) / 2
     let left = lowerLimit;
 
+    // the x1 (right side) of the equation that is (x0 + x1) / 2
     let right = lowerLimit + deltaX;
 
+    // holds the answer of each equation through the loop
     let answer = 0;
 
+    // holds the complete answer
     let finalAnswer;
+    
+    // if the left value hasn't made it to the upper limit of integration, than continue
     while (left < upperLimit) {
       
+      // calculating the midpoint at the current loop
       let currentMidpoint = (left + right) / 2
-      // TODO: fix math.evaluate, its being shown as not a function 
-      answer += math.evaluate({ x: currentMidpoint });
-
+      answer += equation.evaluate({ x: currentMidpoint });
+      // advancing the left and right values for the next loop
       left = right;
-      right = right + h;
+      right = right + deltaX;
 
     }
     
+    // calculating the final answer, which is deltax * all the midpoints added up
     finalAnswer = deltaX * answer;
     if (isNegative) {
       finalAnswer *= -1;
